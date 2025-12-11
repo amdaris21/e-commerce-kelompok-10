9e39f7bc4f4fa6ca92b51815831e97e56b06b69c
Daris <aghnianmaela22@gmail.com>
2025-12-11 20:25:23 +0700

    perbaikan setelah merge checkout

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Seller\ProductCategoryController;
 use App\Http\Controllers\Seller\ProductController;
-use App\Http\Controllers\Customer\CustomerHomeController;
 use App\Http\Controllers\Customer\TransactionController;
 
-
-
-Route::get('/', [CustomerHomeController::class, 'index'])->name('customer.home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -27,15 +23,11 @@ Route::middleware(['auth', 'verified'])->prefix('seller/dashboard')->name('selle
     Route::resource('products', ProductController::class);
 });
 
-Route::get('/search', [CustomerHomeController::class, 'search'])->name('customer.search');
-
-Route::get('/products/{id}', [CustomerHomeController::class, 'show'])->name('customer.product.show');
 
 Route::get('/transaction/{product}', [TransactionController::class, 'show'])->name('transaction.show');
 Route::post('/transaction/process', [TransactionController::class, 'process'])->name('transaction.process')->middleware('auth');
 
 Route::get('/transactions/{transaction}', [TransactionController::class, 'detail'])->name('transaction.detail')->middleware('auth');
 
-Route::post('/cart', [\App\Http\Controllers\CartController::class, 'store'])->name('cart.store')->middleware('auth');
 
 require __DIR__ . '/auth.php';
